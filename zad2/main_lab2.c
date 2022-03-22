fdaa0d54882b33cc4a89bb21ba6e9738c5fa9725
mslazak <marek.slazak@relayr.io>
2022-03-22 01:14:33 +0100

    change to linux style

--- zad2/main_lab2.c
+++ zad2/main_lab2.c
@@ -22,9 +22,10 @@ int is_in_circle(const struct point *p) {
 	return (p->x*p->x + p->y*p->y) < 1;
 }
 
-uint64_t calculate_inside_points(uint64_t num_points) {
-	uint64_t inside = 0;
-	for (uint64_t i = 0; i < num_points; i++) {
+unsigned long long calculate_inside_points(unsigned long long num_points) {
+	unsigned long long inside = 0;
+	unsigned long long i = 0;
+	for (; i < num_points; i++) {
 		struct point p = random_point();
 		if (is_in_circle(&p)) inside++;
 	}
@@ -45,21 +46,21 @@ int main(int argc, char* argv[]) {
 	int world_size;
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-	uint64_t num_points = strtoul(argv[1], NULL, 10);
+	unsigned long long num_points = strtoul(argv[1], NULL, 10);
 	struct timespec tstamp = { 0,0 };
 	struct timespec trecv = { 0,0 };
 	timespec_get(&trecv, TIME_UTC);
 
 
-	uint64_t local_points = num_points / world_size;
+	unsigned long long local_points = num_points / world_size;
 	MPI_Barrier(MPI_COMM_WORLD);
 	srand(time(NULL)+world_rank);
-	uint64_t local_inside = calculate_inside_points(local_points);
+	unsigned long long local_inside = calculate_inside_points(local_points);
 	double local_pi = 4 * ((double)local_inside / (double)local_points);
 
 	printf("Local PI for process %ld is %lf | npoints = %ld\n", world_rank, local_pi, local_points);
 
-	uint64_t global_inside;
+	unsigned long long global_inside;
 	MPI_Reduce(&local_inside, &global_inside, 1, MPI_UINT64_T, MPI_SUM, 0,
 		MPI_COMM_WORLD);
 

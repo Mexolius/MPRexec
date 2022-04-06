a4776221e708d6901b6032e8e117170d591e7140
Marek Ślązak <mslazak99@gmail.com>
2022-04-06 20:08:08 +0200

    Add plot script and results. Modify C++ code

--- zad3/main_lab3.cpp
+++ zad3/main_lab3.cpp
@@ -1,6 +1,8 @@
 #include <omp.h>
 #include <iostream>
 #include <random>
+#include <string>
+#include <memory>
 
 const int64_t SIZE = 100000000;
 
@@ -12,32 +14,118 @@ void print_arr(int *arr, const int size) {
 	}
 }
 
-int main() {
-	int* values;
-	values = new int[SIZE]();
-	/*# pragma omp parallel 
+void randomize_static(float *values, size_t size, int thread_num, int chunk_size) {
+	std::random_device rd;
+	#pragma omp parallel num_threads(thread_num) shared(values)
 	{
-		printf("Thread rank: %d\n", omp_get_thread_num());
-	}*/
+		std::mt19937 generator(rd());
+		std::uniform_real_distribution<float> distribution(0, 200);
 
+		#pragma omp for schedule(static, chunk_size)
+		for (int64_t i = 0; i < size; i++) {
+			values[i] = distribution(generator);
+		}
+	}
+}
+
+void randomize_dynamic(float *values, size_t size, int thread_num, int chunk_size) {
+#pragma omp parallel num_threads(thread_num) shared(values)
+	{
+		std::random_device rd;
+		std::uniform_real_distribution<float> distribution(0, 200);
+		std::mt19937 generator(rd());
+
+#pragma omp for schedule(dynamic, chunk_size)
+		for (int64_t i = 0; i < size; i++) {
+			values[i] = distribution(generator);
+		}
+	}
+}
+
+void randomize_guided(float *values, size_t size, int thread_num, int chunk_size) {
+#pragma omp parallel num_threads(thread_num) shared(values)
+	{
+		std::random_device rd;
+		std::uniform_real_distribution<float> distribution(0, 200);
+		std::mt19937 generator(rd());
+
+#pragma omp for schedule(guided, chunk_size)
+		for (int64_t i = 0; i < size; i++) {
+			values[i] = distribution(generator);
+		}
+	}
+}
+
+void randomize_runtime(float *values, size_t size, int thread_num, int _) {
+#pragma omp parallel num_threads(thread_num) shared(values)
+	{
+		std::random_device rd;
+		std::uniform_real_distribution<float> distribution(0, 200);
+		std::mt19937 generator(rd());
+
+#pragma omp for schedule(runtime)
+		for (int64_t i = 0; i < size; i++) {
+			values[i] = distribution(generator);
+		}
+	}
+}
+
+typedef void(*fun_t)(float*, size_t, int, int);
+
+struct cheat
+{
+	fun_t fun;
+	bool use_chunks;
+	string name;
+	cheat(fun_t f, string name, bool use_chunks = true) : use_chunks(use_chunks), name(name) { fun = f; }
+	void operator ()(float* v0, size_t v1, int v2, int v3)
+	{
+		fun(v0, v1, v2, v3);
+	}
+};
+
+int main() {
 	double start, end;
 
-	
-	for (int threads = 1; threads < 5; threads++) {
-		start = omp_get_wtime();
-		#pragma omp parallel num_threads(threads) shared(values)
-		{
-			std::random_device rd;
-			std::uniform_int_distribution<int> distribution(0, 200);
-			std::mt19937 generator(rd());
-		
-			#pragma omp for schedule(runtime)
-			for (int64_t i = 0; i < SIZE; i++) {
-				values[i] = distribution(generator);
+	const int size_exponents[] = { 16,17,18,19,20,21,22,23 };
+	const int chunk_size_exponents[] = { 2,3,4,5,6 };
+	cout << "Schedule" << ',' << "Threads" << ',' << "Size" << ',' << "Chunk size" << ',' << "Time" << endl;
+
+	const int repeats = 25;
+
+	cheat schedules[] = {
+		{randomize_static,"static"},
+		{randomize_dynamic,"dynamic"},
+		{randomize_guided,"guided"},
+		{randomize_runtime, "runtime", false}
+	};
+	for (const int &size_exp : size_exponents) {
+		size_t size = pow(2, size_exp);
+		unique_ptr<float[]> values = std::make_unique<float[]>(size);
+
+		for (auto &schedule : schedules) {
+			for (int threads = 1; threads < 5; threads++) {
+				if (schedule.use_chunks) {
+					for (const auto &chunk_exp : chunk_size_exponents) {
+						int chunk_size = pow(2, chunk_exp);
+
+						start = omp_get_wtime();
+						for(int i=0;i<repeats;i++)
+							schedule(values.get(), size, threads, chunk_size);
+						end = omp_get_wtime();
+						cout << schedule.name << ',' << threads << ',' << size << ',' << chunk_size << ',' << (end - start)/repeats << endl;
+					}
+				}
+				else {
+					start = omp_get_wtime();
+					for (int i = 0; i < repeats; i++)
+						schedule(values.get(), size, threads, 0);
+					end = omp_get_wtime();
+					cout << schedule.name << ',' << threads << ',' << size << ',' << 0 << ',' << (end - start) / repeats << endl;
+				}
 			}
+			
 		}
-		end = omp_get_wtime();
-		cout <<"Threads: "<< threads << " - " << end - start << "s" << endl;
 	}
 
 	//print_arr(values, SIZE);
---
+++ zad3/plot.py
@@ -0,0 +1,193 @@
+import pandas as pd
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def heatmap(data, row_labels, col_labels, ax=None,
+            cbar_kw={}, cbarlabel="", **kwargs):
+    """
+    Create a heatmap from a numpy array and two lists of labels.
+
+    Parameters
+    ----------
+    data
+        A 2D numpy array of shape (M, N).
+    row_labels
+        A list or array of length M with the labels for the rows.
+    col_labels
+        A list or array of length N with the labels for the columns.
+    ax
+        A `matplotlib.axes.Axes` instance to which the heatmap is plotted.  If
+        not provided, use current axes or create a new one.  Optional.
+    cbar_kw
+        A dictionary with arguments to `matplotlib.Figure.colorbar`.  Optional.
+    cbarlabel
+        The label for the colorbar.  Optional.
+    **kwargs
+        All other arguments are forwarded to `imshow`.
+    """
+
+    if not ax:
+        ax = plt.gca()
+
+    # Plot the heatmap
+    im = ax.imshow(data, **kwargs)
+
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+
+    # Show all ticks and label them with the respective list entries.
+    ax.set_xticks(np.arange(data.shape[1]), labels=col_labels)
+    ax.set_yticks(np.arange(data.shape[0]), labels=row_labels)
+
+    # Let the horizontal axes labeling appear on top.
+    ax.tick_params(top=True, bottom=False,
+                   labeltop=True, labelbottom=False)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=-30, ha="right",
+             rotation_mode="anchor")
+
+    # Turn spines off and create white grid.
+    ax.spines[:].set_visible(False)
+
+    ax.set_xticks(np.arange(data.shape[1] + 1) - .5, minor=True)
+    ax.set_yticks(np.arange(data.shape[0] + 1) - .5, minor=True)
+    ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
+    ax.tick_params(which="minor", bottom=False, left=False)
+
+    return im, cbar
+
+
+def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
+                     textcolors=("black", "white"),
+                     threshold=None, **textkw):
+    """
+    A function to annotate a heatmap.
+
+    Parameters
+    ----------
+    im
+        The AxesImage to be labeled.
+    data
+        Data used to annotate.  If None, the image's data is used.  Optional.
+    valfmt
+        The format of the annotations inside the heatmap.  This should either
+        use the string format method, e.g. "$ {x:.2f}", or be a
+        `matplotlib.ticker.Formatter`.  Optional.
+    textcolors
+        A pair of colors.  The first is used for values below a threshold,
+        the second for those above.  Optional.
+    threshold
+        Value in data units according to which the colors from textcolors are
+        applied.  If None (the default) uses the middle of the colormap as
+        separation.  Optional.
+    **kwargs
+        All other arguments are forwarded to each call to `text` used to create
+        the text labels.
+    """
+
+    if not isinstance(data, (list, np.ndarray)):
+        data = im.get_array()
+
+    # Normalize the threshold to the images color range.
+    if threshold is not None:
+        threshold = im.norm(threshold)
+    else:
+        threshold = im.norm(data.max()) / 2.
+
+    # Set default alignment to center, but allow it to be
+    # overwritten by textkw.
+    kw = dict(horizontalalignment="center",
+              verticalalignment="center")
+    kw.update(textkw)
+
+    # Get the formatter in case a string is supplied
+    if isinstance(valfmt, str):
+        valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
+
+    # Loop over the data and create a `Text` for each "pixel".
+    # Change the text's color depending on the data.
+    texts = []
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            kw.update(color=textcolors[int(im.norm(data[i, j]) > threshold)])
+            text = im.axes.text(j, i, valfmt(data[i, j], None), **kw)
+            texts.append(text)
+
+    return texts
+
+
+def draw_heatmap(threads, chunk_size, data, title, fmt, schedule, size, type, cbarlabel):
+    reshape_tuple = len(threads), len(chunk_size)
+    fig, ax = plt.subplots()
+    plt.title(title, loc='right')
+    plt.xlabel("Chunk size")
+    plt.ylabel("Number of threads")
+    new_data = np.reshape(np.array(data), reshape_tuple)
+    im, cbar = heatmap(new_data, threads, chunk_size, ax=ax, cmap='YlGn', cbarlabel=cbarlabel)
+
+    texts = annotate_heatmap(im, valfmt=fmt)
+
+    fig.tight_layout()
+    plt.savefig(f'./results/{schedule}/{type}_{size}.png')
+    plt.close(fig)
+
+
+def plot(threads, chunk_size, time, speedup, schedule, size):
+    ## TIME
+    draw_heatmap(threads, chunk_size, time * (10 ** 6),
+                 f"Time for {schedule} with {size} values", "{x:.0f} ms"
+                 , schedule, size, "time", "time [ms]")
+    ## SPEEDUP
+    draw_heatmap(threads, chunk_size, speedup,
+                 f"Speedup for {schedule} with {size} values", "{x:.4f}"
+                 , schedule, size, "speedup", "speedup value")
+
+
+def add_speedup(data: pd.DataFrame):
+    speedups = []
+
+    times_for_one_proc = data.groupby(['Schedule', 'Size', 'Chunk size']).first()['Time']
+    for index, record in data.iterrows():
+        time_for_one = times_for_one_proc[record['Schedule']][record['Size']][record['Chunk size']]
+        speedups.append(time_for_one / record['Time'])
+
+    data['Speedup'] = speedups
+
+
+def create_dirs():
+    import os
+
+    paths = [
+        './results',
+        './results/static',
+        './results/dynamic',
+        './results/guided',
+        './results/runtime',
+    ]
+
+    for path in paths:
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+
+if __name__ == "__main__":
+    data = pd.read_csv('./out.csv')
+
+    create_dirs()
+
+    schedules = data['Schedule'].unique()
+    sizes = data['Size'].unique()
+
+    add_speedup(data)
+
+    grouped = data.groupby(['Schedule', 'Size'])
+
+    for schedule in schedules:
+        for size in sizes:
+            grouping = grouped.get_group((schedule, size))
+            plot(grouping['Threads'].unique(), grouping['Chunk size'].unique(), grouping['Time'], grouping['Speedup'],
+                 schedule, size)
